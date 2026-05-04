e82609f05713ebf91ba4d2b91d84fa6d14242a03
Mike Montague <mikemon@gmail.com>
2026-05-03 18:49:13 -0700

    move sock filter to child config

--- child.go
+++ child.go
@@ -23,9 +23,10 @@ const (
 )
 
 type childConfig struct {
-	Path string
-	Args []string
-	Env  []string
+	Path   string
+	Args   []string
+	Env    []string
+	Filter []unix.SockFilter
 }
 
 func recvConfig(fd int) (*childConfig, error) {
@@ -44,68 +45,14 @@ func recvConfig(fd int) (*childConfig, error) {
 	return &cfg, nil
 }
 
-func installListener() int {
-	prog := []unix.SockFilter{
-		// Reject anything that isn't x86-64 native; 32-bit compat (int 0x80) uses
-		// different syscall numbers that the rest of the filter never sees.
-		{Code: unix.BPF_LD | unix.BPF_W | unix.BPF_ABS, K: 4}, // load seccomp_data.arch
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.AUDIT_ARCH_X86_64, Jt: 1, Jf: 0},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_KILL_PROCESS},
-
-		{Code: unix.BPF_LD | unix.BPF_W | unix.BPF_ABS, K: 0}, // load seccomp_data.nr
-
-		// clone
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_CLONE, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// clone3
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_CLONE3, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// execve
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_EXECVE, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// execveat
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_EXECVEAT, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// fork
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_FORK, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// open
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_OPEN, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// openat
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_OPENAT, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// openat2
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_OPENAT2, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		// open_by_handle_at
-		// No pathname is available from the file handle; deny unconditionally.
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_OPEN_BY_HANDLE_AT, Jt: 0,
-			Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_KILL_PROCESS},
-
-		// vfork
-		{Code: unix.BPF_JMP | unix.BPF_JEQ | unix.BPF_K, K: unix.SYS_VFORK, Jt: 0, Jf: 1},
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_USER_NOTIF},
-
-		{Code: unix.BPF_RET | unix.BPF_K, K: unix.SECCOMP_RET_ALLOW},
-	}
-
-	fprog := unix.SockFprog{
-		Len:    uint16(len(prog)),
-		Filter: &prog[0],
+func installListener(sf []unix.SockFilter) int {
+	sfp := unix.SockFprog{
+		Len:    uint16(len(sf)),
+		Filter: &sf[0],
 	}
 
 	fd, _, errno := unix.Syscall(unix.SYS_SECCOMP, unix.SECCOMP_SET_MODE_FILTER,
-		unix.SECCOMP_FILTER_FLAG_NEW_LISTENER, uintptr(unsafe.Pointer(&fprog)))
+		unix.SECCOMP_FILTER_FLAG_NEW_LISTENER, uintptr(unsafe.Pointer(&sfp)))
 	if errno != 0 {
 		fmt.Fprintf(os.Stderr, "seccomp(SET_MODE_FILTER, NEW_LISTENER): %d", errno)
 		os.Exit(childNewListenerFailed)
@@ -153,19 +100,19 @@ func init() {
 		os.Exit(childNoNewPrivsFailed)
 	}
 
-	fd := installListener()
+	cfg, err := recvConfig(childSocketFd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "sandbox child: recv config: %s\n", err)
+		os.Exit(childRecvConfigFailed)
+	}
+
+	fd := installListener(cfg.Filter)
 	unix.CloseOnExec(fd)
 
 	err = unix.Sendmsg(childSocketFd, nil, unix.UnixRights(fd), nil, 0)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "sandbox child: sendmsg: %s\n", err)
 		os.Exit(childSendmsgFailed)
-	}
-
-	cfg, err := recvConfig(childSocketFd)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "sandbox child: recv config: %s\n", err)
-		os.Exit(childRecvConfigFailed)
 	}
 
 	err = unix.Exec(cfg.Path, cfg.Args, cfg.Env)
