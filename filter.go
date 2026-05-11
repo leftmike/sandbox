64c7e3d3431de047c0569b52e1c859ff33572bc9
Mike Montague <mikemon@gmail.com>
2026-05-11 09:25:28 -0700

    add default deny list based on systemd and moby

--- filter.go
+++ filter.go
@@ -1,8 +1,3 @@
-/*
-https://github.com/systemd/systemd/blob/main/src/shared/seccomp-util.c
-https://github.com/flatpak/flatpak/blob/main/common/flatpak-run.c
-https://github.com/moby/profiles/blob/main/seccomp/default.json
-*/
 package main
 
 import (
@@ -78,9 +73,109 @@ var (
 		"open":     {Action: unix.SECCOMP_RET_USER_NOTIF},
 		"openat":   {Action: unix.SECCOMP_RET_USER_NOTIF},
 		"openat2":  {Action: unix.SECCOMP_RET_USER_NOTIF},
+		"vfork":    {Action: unix.SECCOMP_RET_USER_NOTIF},
+
 		// No pathname is available from the file handle; deny unconditionally.
 		"open_by_handle_at": {Action: unix.SECCOMP_RET_KILL_PROCESS},
-		"vfork":             {Action: unix.SECCOMP_RET_USER_NOTIF},
+
+		// Default deny list: returns EPERM. Based on:
+		// https://github.com/systemd/systemd/blob/main/src/shared/seccomp-util.c
+		// https://github.com/moby/profiles/blob/main/seccomp/default.json
+
+		// Kernel module manipulation
+		"init_module":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"finit_module":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"delete_module":   FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"create_module":   FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"get_kernel_syms": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"query_module":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Kexec and reboot
+		"kexec_load":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"kexec_file_load": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"reboot":          FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Mounts, filesystem context, and namespace switching
+		"mount":         FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"umount2":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"pivot_root":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"chroot":        FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"fsopen":        FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"fsconfig":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"fsmount":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"fspick":        FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"move_mount":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"open_tree":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"mount_setattr": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"unshare":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"setns":         FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Clock and time
+		"settimeofday":  FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"clock_settime": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"clock_adjtime": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"adjtimex":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Hostname and domain
+		"sethostname":   FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"setdomainname": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Quotas and swap
+		"quotactl":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"quotactl_fd": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"swapon":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"swapoff":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Kernel programmability and performance counters
+		"bpf":             FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"perf_event_open": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Cross-process inspection and tracing
+		"ptrace":            FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"process_vm_readv":  FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"process_vm_writev": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"process_madvise":   FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"kcmp":              FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Process accounting: obsolete or rarely-needed
+		"acct":           FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"nfsservctl":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"vhangup":        FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"lookup_dcookie": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// userfaultfd and io_uring
+		"userfaultfd":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"io_uring_setup":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"io_uring_enter":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"io_uring_register": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// Kernel keyring
+		"add_key":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"request_key": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"keyctl":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// NUMA memory policy
+		"mbind":                   FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"migrate_pages":           FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"move_pages":              FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"set_mempolicy":           FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"get_mempolicy":           FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"set_mempolicy_home_node": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+
+		// amd64-only legacy and obsolete syscalls (ignored on other arches)
+		"iopl":        FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"ioperm":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"_sysctl":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"afs_syscall": FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"tuxcall":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"vserver":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"getpmsg":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"putpmsg":     FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"security":    FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"uselib":      FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"ustat":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"sysfs":       FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
+		"modify_ldt":  FilterConfig{Action: unix.SECCOMP_RET_ERRNO, Errno: unix.EPERM},
 	}
 
 	defaultSockFilter = makeSockFilter(defaultFilterConfig)
